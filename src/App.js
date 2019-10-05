7052b4eb0a4ba0ac4ac5efdaf5cc37a226e648cf
Eddie-Z <35039865+Eddie-Z@users.noreply.github.com>
2019-10-05 14:43:52 -0700

    Todo states

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from 'react';
+import Todos from './components/Todos';
 
-function App() {
-  return (
-    <div className="App">
-      <n1>App</n1>
-    </div>
-  );
+class App extends Component {
+  state = {
+    todos:[
+      {
+        id:1,
+        title: 'take out the trash',
+        complete:false
+      },
+      {
+        id:2,
+        title: 'eat breakfast',
+        complete:false
+      },
+      {
+        id:3,
+        title: 'eat dinner',
+        complete:false
+      }
+    ]
+  }
+  
+  render() {
+      console.log(this.state.todos)
+      return(
+      <div className="App">
+        <Todos/>
+      </div>
+    );
+  }
 }
 
 export default App;
